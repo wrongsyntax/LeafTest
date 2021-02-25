9e5fd5cdb99b698e8e4979617c86da7c2bea79e4
Uzair <uzair0213@icloud.com>
2021-02-24 22:07:37 -0700

    Changed port to 1227

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -3,9 +3,17 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) throws {
-    // uncomment to serve files from /Public folder
-     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    // Set port to 1227
+    app.http.server.configuration.port = 1227
 
+    // uncomment to serve files from /Public folder
+    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+
+/*
+    if !app.environment.isRelease {
+        LeafRenderer.Option.caching = .bypass
+    }
+*/
     // This tells Vapor to use the LeafRenderer when you call req.view in your code.
     app.views.use(.leaf)
 
